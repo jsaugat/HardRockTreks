e3b509aaed2573f4ea6e81fa7832aef3dba0b37a
Saugat Joshi <jsaugatt02@gmail.com>
2024-10-29 17:56:32 +0545

    delete some files

--- components/popular-destinations/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function PopularDestinations() {
-  return (
-    <div className='h-screen bg-red-400 flex items-center justify-center'>
-      dfdfd
-    </div>
-  )
-}
