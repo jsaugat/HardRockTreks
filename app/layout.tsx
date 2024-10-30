a7ca795e7488d1c45d725a50b0f9704d2300222d
Saugat Joshi <jsaugatt02@gmail.com>
2024-10-31 00:01:06 +0545

    add: local neue Regrade font using next js principles

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,36 @@ import { GeistSans } from 'geist/font/sans';
 import Navbar from '@/components/header/Navbar';
 import { Toaster } from '@/components/ui/sonner';
 import { TopLoadingBarProvider } from '@/contexts/TopLoadingBar';
+import Footer from '@/components/footer';
+import localFont from "next/font/local";
 
 // const inter = Inter({ subsets: ['latin'] });
+// Local Font
+const neueRegrade = localFont({
+  src: [
+    {
+      path: "../public/fonts/Neue Regrade/Neue-Regrade-Regular-BF65af35d81f2ff.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/Neue Regrade/Neue-Regrade-Medium-BF65af35d843ed6.otf",
+      weight: "500",
+      style: "medium",
+    },
+    {
+      path: "../public/fonts/Neue Regrade/Neue-Regrade-Semibold-BF65af35d8354a8.otf",
+      weight: "600",
+      style: "semibold",
+    },
+    {
+      path: "../public/fonts/Neue Regrade/Neue-Regrade-Bold-BF65af35d84e111.otf",
+      weight: "700",
+      style: "bold",
+    },
+  ],
+  variable: "--font-neueRegrade",
+});
 
 export const metadata: Metadata = {
   title: 'Hard Rock Treks & Expeditions',
@@ -20,13 +48,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={GeistSans.className}>
+      <body className={`${GeistSans.className} ${neueRegrade.variable}`}>
         <TopLoadingBarProvider>
           <div className="min-h-screen bg-secondary relative">
             <Navbar />
             <main className="container mx-auto relative z-0">
               {children}
             </main>
+            <Footer />
           </div>
           <Toaster />
         </TopLoadingBarProvider>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        neueRegrade: ["var(--font-neueRegrade)"],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
