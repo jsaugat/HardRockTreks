4a1f18ecbf21dead7e75cb6ca99c0a18af983949
Saugat Joshi <jsaugatt02@gmail.com>
2024-10-30 12:17:11 +0545

    ,

--- contexts/TopLoadingBar.tsx
+++ contexts/TopLoadingBar.tsx
@@ -4,24 +4,28 @@ import React, { createContext, useContext, useState, useEffect, ReactNode } from
 import LoadingBar from 'react-top-loading-bar';
 import { usePathname } from 'next/navigation';
 
+//? Define the type of the context
 type TopLoadingBarContextType = {
   triggerLoading: () => void;
 };
 
+//? Create the context
 const TopLoadingBarContext = createContext<TopLoadingBarContextType | undefined>(undefined);
 
+//? Provider component for the context
 export const TopLoadingBarProvider = ({ children }: { children: ReactNode }) => {
   const [progress, setProgress] = useState(0);
   const pathname = usePathname();
   const randomProgress = Math.floor(Math.random() * 60); // Generate a random value below 60
 
+  // Function to trigger the loading bar
   const triggerLoading = () => {
     setProgress(randomProgress);
     setTimeout(() => setProgress(100), 1000);
   };
 
+  // Effect to trigger the loading bar on route change
   useEffect(() => {
-    // Trigger loading bar on route change
     setProgress(randomProgress);
     const handleRouteChangeComplete = () => setProgress(100);
 
@@ -29,15 +33,16 @@ export const TopLoadingBarProvider = ({ children }: { children: ReactNode }) => 
     return () => setProgress(0);
   }, [pathname]);
 
+  // Render the loading bar and the children
   return (
     <TopLoadingBarContext.Provider value={{ triggerLoading }}>
-      <LoadingBar color="#f97316" progress={progress} onLoaderFinished={() => setProgress(0)} />
+      <LoadingBar color="skyblue" progress={progress} onLoaderFinished={() => setProgress(0)} />
       {children}
     </TopLoadingBarContext.Provider>
   );
 };
 
-// Custom hook to use the loading bar context
+//? Custom hook to use the loading bar context
 export const useLoadingBar = () => {
   const context = useContext(TopLoadingBarContext);
   if (!context) {
