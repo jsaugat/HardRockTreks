98f9fab5a0bc0656c166955a27a96e7ab1d95e86
Saugat Joshi <jsaugatt02@gmail.com>
2024-10-30 18:52:11 +0545

    add: logic to implement hide/show logo text on scroll up or down

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,20 +1,44 @@
 import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export function Logo({ className }: { className?: string }) {
+  const [isVisible, setIsVisible] = useState(true); // Logo is visible by default
+  const [lastScrollTop, setLastScrollTop] = useState(0); // Initial scroll position
+
+  /**
+   *? Logic to Hide/Show logo text on scroll down/up :-
+   * Scroll down = Hide logo text
+   * Scroll up = Show logo text
+   */
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollTop = document.documentElement.scrollTop;
+      setIsVisible(currentScrollTop <= lastScrollTop);
+      setLastScrollTop(currentScrollTop);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollTop]);
+
   return (
-    <Link href="/">
+    <Link href="/" className=''>
       <main className='flex items-center gap-3 relative'>
+        {/* LOGO ICON */}
         <Image
           src='/logo.png'
           alt='Hard Rock Treks & Expeditions'
           width={60}
           height={60}
         />
-        <div className={cn("relative flex flex-col items-start justify-start gap-0 p-0 font-semibold text-secondary uppercase text-2xl leading-none mix-blend-difference", className)}>
-          <span className='mix-blend-difference'>
+        {/* LOGO TEXT */}
+        <div className={cn("relative flex flex-col items-start justify-start gap-0 p-0 font-semibold text-secondary uppercase text-2xl leading-none", className, isVisible ? "opacity-100" : "opacity-0 pointer-events-none")}>
+          <span className=''>
             Hard Rock Treks & Expeditions
           </span>
           <span className='font-normal text-base'>
@@ -25,3 +49,4 @@ export function Logo({ className }: { className?: string }) {
     </Link>
   )
 }
+
