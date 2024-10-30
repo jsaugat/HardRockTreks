8dad779b8f8a499dbcac7952bb455705b61e2c7d
Saugat Joshi <jsaugatt02@gmail.com>
2024-10-31 02:33:40 +0545

    .

--- components/home/ContentTemplate.tsx
+++ components/home/ContentTemplate.tsx
@@ -25,7 +25,7 @@ export function ContentTemplate({
       <section className="flex items-end justify-between w-full">
         <header>
           {/* Title */}
-          <h1 className='text-3xl sm:text-4xl lg:text-6xl font-bold uppercase font-neueRegrade tracking-tight bg-gradient-to-br from-primary via-primary to-foreground bg-clip-text text-transparent'>
+          <h1 className='text-3xl sm:text-4xl lg:text-6xl font-bold uppercase font-neueRegrade tracking-tight bg-gradient-to-br from-primary via-purple-400 to-purple-400 bg-clip-text text-transparent'>
             {title}
           </h1>
           {/* Subtitle */}
